7a240a3090687d55a45df52eb2860d9f4ad5f057
Elshorihy <sheenomatp@gmail.com>
2026-09-11 18:11:49 +0300

    Add server profile model

---
+++ app/src/main/java/com/elshorihy/tunnle/ServerProfile.kt
@@ -0,0 +1,11 @@
+package com.elshorihy.tunnle
+
+data class ServerProfile(
+    val id: String,
+    val name: String,
+    val country: String,
+    val host: String,
+    val port: Int,
+    val protocol: String,
+    val note: String = ""
+)
